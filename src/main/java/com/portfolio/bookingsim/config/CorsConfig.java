301fb318bd9bb2d8a5a3094db03bd29ed2ae9663
Chabelly Lezcano <chabelly370@gmail.com>
2026-02-07 18:08:19 +0100

    CORS problem solved. Added configuration

--- src/main/java/com/portfolio/bookingsim/config/CorsConfig.java
+++ src/main/java/com/portfolio/bookingsim/config/CorsConfig.java
@@ -7,16 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200", "https://sim-booking-frontend.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+          .allowedOriginPatterns(
+            "http://localhost:4200",
+            "https://*.vercel.app"
+          )
+          .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+          .allowedHeaders("*");
+      }
+    };
+  }
 }
