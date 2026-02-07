10e9115d990e5d7da0d3cbb83f79ac4a804969b3
Chabelly Lezcano <chabelly370@gmail.com>
2026-02-07 18:03:08 +0100

    CORS problem solved. Added configuration

--- src/main/java/com/portfolio/bookingsim/service/BookingService.java
+++ src/main/java/com/portfolio/bookingsim/service/BookingService.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -159,4 +160,9 @@ public class BookingService {
                 b.getTotalPrice()
         );
     }
+
+     @GetMapping("/health")
+  public String health() {
+    return "ok";
+  }
 }
